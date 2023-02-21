151711c3a3a98bfc080feabbcd802251a30e1bbb
Ezequiel Macías Rivera <ezequielmr678@gmail.com>
2023-02-21 13:58:59 -0600

    Added baseline service

---
+++ src/main/java/practicascloud/baselineservice/BaselineServiceApplication.java
@@ -0,0 +1,13 @@
+package practicascloud.baselineservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaselineServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaselineServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/practicascloud/baselineservice/ServletInitializer.java
@@ -0,0 +1,13 @@
+package practicascloud.baselineservice;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BaselineServiceApplication.class);
+	}
+
+}
